ac4c5117411f2053e1a4e47870d2a95f641faba7
Paul <paul.richez59@gmail.com>
2021-10-23 12:08:38 +0200

    conding foreach with file replacement

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,22 +1,48 @@
 const loader = document.getElementById('loader');
 
 window.addEventListener('load', () => {
-    setTimeout(() => { loader.remove(); setVariableInHtml(); }, 1000);
+    setTimeout(async () => { loader.remove(); await setVariableInHtml(); }, 1000);
 });
 
-function setVariableInHtml() {
+async function setVariableInHtml() {
     const age = calculate_age(new Date(myCvData.user.birthDay)) + " Ans";
     myCvData.user.birthDay = age;
     for (var key in myCvData.user) {
-        if (myCvData.user.hasOwnProperty(key)) {         
+        if (myCvData.user.hasOwnProperty(key)) {
             document.body.innerHTML = document.body.innerHTML.replaceAll(`{{${key}}}`, `${myCvData.user[key]}`);
         }
     }
-   }
 
-function calculate_age(dob) { 
+    const condingHTML = await readTextFile("/assets/html/coding.html").then((text) => text);
+    myCvData.coding.forEach(t => {
+        document.getElementById('coding').appendChild(htmlToElement(replaceCoding(condingHTML, t.key, t.value)));
+    });
+}
+
+function calculate_age(dob) {
     var diff_ms = Date.now() - dob.getTime();
-    var age_dt = new Date(diff_ms); 
-  
+    var age_dt = new Date(diff_ms);
+
     return Math.abs(age_dt.getUTCFullYear() - 1970);
+}
+
+function readTextFile(file) {
+    return fetch('/assets/html/coding.html')
+        .then(function (response) {
+            return response.text();
+        })
+        .catch(function (err) {
+            return null;
+        });
+}
+
+function htmlToElement(html) {
+    var template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
+
+function replaceCoding(html, key, value) {
+    return html.replaceAll("{{key}}", key).replaceAll("{{value}}", value)
 }
