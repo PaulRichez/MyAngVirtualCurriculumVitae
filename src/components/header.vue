6aeb512d38870ce64cb1da89e3a781d9807953b7
PaulRichez <paul.richez59@gmail.com>
2022-11-18 05:40:27 +0100

    header click sidenav mobile toggle

--- src/components/header.vue
+++ src/components/header.vue
@@ -17,14 +17,32 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mr-5 mb-lg-0">
           <li class="nav-item">
-            <router-link class="nav-link" to="/">Hello</router-link>
+            <router-link class="nav-link" to="/">
+              <span
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+              >
+                Hello
+              </span>
+            </router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/resume">Mon CV</router-link>
+            <router-link class="nav-link" to="/resume">
+              <span
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                >Mon CV
+              </span>
+            </router-link>
           </li>
           <li class="nav-item">
             <router-link class="nav-link" to="/portfolio">
-              Portfolio
+              <span
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+              >
+                Portfolio
+              </span>
             </router-link>
           </li>
         </ul>
