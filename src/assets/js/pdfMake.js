c15ef1fd8af51d7a838fb5d5b5146e51c3f59fb5
PaulRichez <paul.richez59@gmail.com>
2022-11-19 07:10:42 +0100

    pdf bundle size reduce

--- src/assets/js/pdfMake.js
+++ src/assets/js/pdfMake.js
@@ -1,8 +1,53 @@
 import pdfMake from "pdfmake/build/pdfmake.min";
-import pdfFonts from "pdfmake/build/vfs_fonts";
 import { myCvData } from '../data/data';
 import theme from './getTheme';
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
+function fetchFont(fontURL) {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest()
+        request.open('GET', fontURL, true)
+        request.responseType = 'arraybuffer'
+
+        request.onload = function () {
+            resolve(request.response)
+        }
+
+        request.onerror = reject
+
+        request.send()
+    })
+}
+
+class PdfFontLoader {
+    constructor() {
+        this.fontDefs = []
+        this.vfs = {}
+    }
+    addFont(fontDef) {
+        this.fontDefs.push(fontDef)
+    }
+
+    load() {
+        return new Promise((resolve, reject) => {
+            if (this.loaded) {
+                resolve()
+            } else {
+                const fetches = this.fontDefs.map(fontDef => {
+                    return fetchFont(fontDef.URL).then((data) => {
+                        this.vfs[fontDef.name] = data
+                    })
+                })
+                Promise.all(fetches).then(() => {
+                    this.loaded = true
+                    resolve()
+                }).catch(reject)
+            }
+        })
+    }
+}
+
+export const fontLoader = new PdfFontLoader()
+
+pdfMake.vfs = fontLoader.vfs
 
 const age = new Date(new Date() - new Date(myCvData.user.birthDay)).getFullYear() -
     1970;
